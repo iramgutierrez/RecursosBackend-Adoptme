0fffb5c0de7d85e096e76cad6ec0de8b2c629196
Iram Gutiérrez <iram.gutierrez@rappi.com>
2023-11-23 19:17:38 -0600

    Add pets view

--- src/app.js
+++ src/app.js
@@ -6,13 +6,29 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import handlebars from 'express-handlebars';
+import { petsService } from "./services/index.js";
+import __dirname from './utils/index.js'
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
 const connection = mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use(cookieParser());
+
+// Configuración handlebars
+app.engine('handlebars', handlebars.engine())
+app.set('views', './src/views')
+app.set('view engine', 'handlebars')
+
+app.get('/pets', async (req, res) => {
+  let pets = await petsService.getAll();
+  pets = pets.map(pet => pet.toObject())
+  return res.render('pets', { pets })
+})
 
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
